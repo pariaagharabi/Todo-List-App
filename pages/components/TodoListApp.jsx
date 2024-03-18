000415b006042e64b6ac48a956a310801764fd04
PARIA AGHARABI <pariaagharabi@gmail.com>
2024-03-18 09:59:55 -0700

    Add Styling for the App

--- pages/components/TodoListApp.jsx
+++ pages/components/TodoListApp.jsx
@@ -3,7 +3,21 @@ import React from 'react';
 const TodoListApp = () => {
     return (
         <>
-        
+            <div className="app-container">
+                <div className="app-inner-cntainer">
+
+                    <div className="app-header-container">
+                        
+                    </div>
+
+                    <div className="todo-list-container"></div>
+
+                    <div className="display-new-todos"></div>
+
+                    <div className="todos-notification"></div>
+
+                </div>
+            </div>
 
         </>
     );
